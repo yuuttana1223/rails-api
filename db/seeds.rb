fea473a3409a504fcc4ddfa1650292c0dfb36c13
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-18 16:04:22 +0900

    🌱 Reviewのレコードを1つ挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Review.create!({ user_id: 1, lecture_name: "プロ演2", teature_name: "前納", lesson_type: "対面", adequacy: "満足", submission_quantity: "多い", difficulty: "難しい", content: "良かったよー" })
