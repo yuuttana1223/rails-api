5b27406240dfa6e25c6808591ad948ad7851db2b
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-18 12:04:56 +0900

    ✨ deviseのログインとサインアップの設定とjsonをレスポンスとして返せるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
-class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+class ApplicationController < ActionController::Base
+  # helper_methodを提供する
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  # CSRFトークン検証をスキップ
+  skip_before_action :verify_authenticity_token
+  helper_method :current_user, :user_signed_in?
 end
