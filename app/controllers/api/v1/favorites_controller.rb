2736c06b0d12fe993d1bc2c2896704c7f5044cf3
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-02-11 15:59:38 +0900

    ✨ お気に入り機能を追加

---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::FavoritesController < ApplicationController
+  before_action :authenticate_api_v1_user!, only: %i[create destroy]
+
+  def index
+    favorites = Favorite.select("user_id", "review_id").as_json(except: %i[id])
+    render json: favorites, status: :ok
+  end
+
+  def create
+    favorite = current_api_v1_user.favorites.build(review_id: params[:review_id])
+    if favorite.save
+      render json: favorite.as_json(only: %i[user_id review_id]), status: :created
+    else
+      render json: favorite.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if current_api_v1_user.favorites.find_by(review_id: params[:review_id]).destroy
+      head :no_content
+    else
+      render json: { error: "Failed to destroy" }, status: :unprocessable_entity
+    end
+  end
+end
