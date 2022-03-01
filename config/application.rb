fb95e62f1430b807af1dbda3048022cdfdff4390
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-03-01 17:28:35 +0900

    🔧 Pryの記述をdevelopmentしか読み込まないように変更

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,5 @@ module Myapp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    Pry.config.pager = false
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,4 +64,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3001 }
+  Pry.config.pager = false
 end
