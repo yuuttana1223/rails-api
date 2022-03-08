330de417e57f74edb2f664e888c4760608b05840
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-03-09 01:16:49 +0900

    🔧 iPhoneでも開発環境を見れるように変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "https://kcg-review.vercel.app"
+    origins "localhost:3000", "http://192.168.0.114:3000", "https://kcg-review.vercel.app"
 
     resource "*",
       headers: :any,
